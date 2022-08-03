edf800a97ee29b1395a11f6e3445eac97f915bc7
Hand_MK <thflgg133@naver.com>
2022-08-03 17:32:21 +0900

    Softeer

--- 복잡한 조립라인1.py
+++ 복잡한 조립라인1.py
@@ -1,1 +1,35 @@
 import sys
+
+K, N = map(int, sys.stdin.readline().split())
+
+dp = []
+travel_time = [[[0 for i in range(K)] for j in range(K)] for k in range(N)]
+
+for i in range(N-1):
+    line = list(map(int, sys.stdin.readline().split()))
+    dp.append(line[:K]) # 작업 시간
+    cnt = 0
+
+    for j in range(K):
+        for k in range(K):
+            if j == k:
+                continue
+
+            travel_time[i][j][k] = line[K + cnt]
+            cnt += 1
+
+# 마지막 라인에는 이동시간이 없으므로 따로 추가   
+dp.append(list(map(int, sys.stdin.readline().split())))
+
+for i in range(N-1):
+    for j in range(K):
+        tmp = dp[i][j]
+        for k in range(K):
+            if j == k: # 같은 라인으로 이동할때는 이동 시간이 안걸리므로 패스
+                continue
+            
+            tmp = min(tmp, dp[i][k] + travel_time[i][k][j])
+        
+        dp[i+1][j] += tmp
+
+print(min(dp[-1]))
